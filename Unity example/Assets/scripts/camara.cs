467b29decd331d1499a8db11bf65284eb565c6db
EsthefanyVelasquez <esthefanyve@gmail.com>
2024-10-11 11:39:20 -0500

    Movimientocamara

--- Unity example/Assets/scripts/camara.cs
+++ Unity example/Assets/scripts/camara.cs
@@ -8,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
 
     public new Transform camera;
     public float speed = 4;
-    public float gravity =-9.8f;
+    public float gravity = -9.8f;
+    public float rotationSpeed = 100f; // Velocidad de rotación del personaje
 
     // Start is called before the first frame update
     void Start()
@@ -19,28 +20,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal"); // Flechas izquierda/derecha
+        float ver = Input.GetAxis("Vertical");   // Flechas adelante/atrás
         Vector3 movement = Vector3.zero;
 
-        if (hor != 0 || ver != 0)
+        // Movimiento de avance
+        if (ver != 0)
         {
             Vector3 forward = camera.forward;
             forward.y = 0;
             forward.Normalize();
 
-            Vector3 right = camera.right;
-            right.y = 0;
-            right.Normalize();
-
-            Vector3 direction = forward * ver + right * hor;
-            direction.Normalize();
-
-            movement = direction * speed * Time.deltaTime;
+            movement = forward * ver * speed * Time.deltaTime;
         }
-        
+
+        // Aplicar la rotación
+        if (hor != 0)
+        {
+            // Rotar el personaje en el eje Y (izquierda o derecha)
+            transform.Rotate(0, hor * rotationSpeed * Time.deltaTime, 0);
+        }
+
+        // Aplicar gravedad
         movement.y += gravity * Time.deltaTime;
 
+        // Mover el personaje
         characterController.Move(movement);
     }
 }
+
