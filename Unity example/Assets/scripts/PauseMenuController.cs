e8626be142e73d7d19a4ca163d12b266457591a8
monjerobertoGit <robertom0310@gmail.com>
2024-10-16 20:23:30 -0500

    Menu de pausa en el juego

---
+++ Unity example/Assets/scripts/PauseMenuController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static PauseMenuController instance;
+    public GameObject pauseMenu;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Esto hace que el objeto persista entre escenas
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; // Pausa el juego
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f; // Reanuda el juego
+        isPaused = false;
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Abrir Configuración");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
