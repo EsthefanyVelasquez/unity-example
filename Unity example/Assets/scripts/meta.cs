8209c2682501d15f9311820a504faffcaae882b9
LeWacho <juan.esteban.jaramilloh@gmail.com>
2024-10-17 17:57:34 -0500

    intento de  hacer  la meta

--- Unity example/Assets/scripts/meta.cs
+++ Unity example/Assets/scripts/meta.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class meta : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Canvas1;
+    public Text txtmeta;
+    private void OnTriggerEnter(Collider Player)
     {
-        
-    }
+        if(Player.name == "Trigger")
+        {
+            Canvas1.SetActive(true);
+            txtmeta.text = "HAS GANADO";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
