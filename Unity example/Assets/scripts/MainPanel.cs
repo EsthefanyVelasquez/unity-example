dc04a36a0bac11c3d4977c5ebd74498e39ec17ea
EsthefanyVelasquez <esthefanyve@gmail.com>
2024-10-30 21:07:24 -0500

    Configuraciones

---
+++ Unity example/Assets/scripts/MainPanel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+ [Header("options")]
+ public Slider volumeFX;
+ public Slider volumeMaster;
+ public Toggle mute;
+ [Header("Panels")]
+ public GameObject mainPanel;
+ public GameObject optionsPanel;
+ public GameObject levelSelectPanel;
+
+ public void OpenPanel( GameObject panel)
+ {
+    mainPanel.SetActive(false);
+    optionsPanel.SetActive(false);
+    levelSelectPanel.SetActive(false);
+
+    panel.SetActive(true);
+ }
+}
